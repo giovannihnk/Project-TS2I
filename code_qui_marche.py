30d00777c8f335af21e7f773cab649955b3cd4a2
emnabnk <153511857+emnabnk@users.noreply.github.com>
2025-01-16 08:48:26 +0100

    Update code_qui_marche.py

--- code_qui_marche.py
+++ code_qui_marche.py
@@ -8,29 +8,26 @@ import matplotlib.image as mpimg
 
 image_path = 'vagues.png'
 
-image_origine = mpimg.imread(image_path)
+image_origine = mpimg.imread(image_path)   #lecture de l'image 
 image = mpimg.imread(image_path)
 
-
+# Convertion en entiers de 0 à 255
 if image.dtype != np.uint8:  # Si les valeurs sont en flottant (0 à 1)
-    image = (image * 255) # Convertir en entiers (0 à 255)
+    image = (image * 255) 
 else:
     image = (image/np.max(image))*255
 
+
+# Tronquer l'image
 hauteur, largeur = image.shape[:2]
 nouvelle_hauteur = (hauteur // 8) * 8
 nouvelle_largeur = (largeur // 8) * 8
-
-# image = image[:,:,0]  #on garde qu'une seule couleur (2D)
-
-
-# Tronquer l'image
 image = image[:nouvelle_hauteur, :nouvelle_largeur,:]
 
+#on centralise les valeurs de la matrice
 image = image - 128
+
 taille_image = image.shape
-#print(taille_image)
-#print(image)
 
 #initialisation de P (matrice de passage) avec la formule de la double somme
 P=np.zeros((8,8))
@@ -46,7 +43,7 @@ for i in range (8):
 
 
 
-# initialisation de Q : matrice de quantification Q dans la norme de compression JPEG
+# initialisation de Q : matrice de quantification Q 
 Q=np.array([[16,11,10,16,24,40,51,61],
             [12,12,13,19,26,58,60,55],
             [14,13,16,24,40,57,69,56],
@@ -64,20 +61,19 @@ Q=np.array([[16,11,10,16,24,40,51,61],
 
 compressed = np.zeros_like(image, dtype=float)
 
-for c in range(taille_image[2]):
+for c in range(taille_image[2]):   #comression pour tous les canaux de couleurs
+    #compression par blocs de 8 
     for i in range(0, taille_image[0], 8):
-        for j in range(0, taille_image[1], 8):
-
+        for j in range(0, taille_image[1], 8):  
             bloc = image[i:i+8, j:j+8,c]
+            #application de la formule de changemment de base 
             D = np.dot(P, np.dot(bloc,P.T))
             D_tilde = np.round(D/Q)
-        #print(D)
-        # prendre la partie entière
             compressed[i:i+8, j:j+8,c] = D_tilde
 
 
 
-#  — Compter le nombre de cœfficients non nuls pour obtenir le taux de compression
+###### TAUX DE COMPRESSION #######
 nb_coeff_non_zero = np.count_nonzero(compressed)  # Nombre de coefficients non nuls
 taux_compression = 100 - ((nb_coeff_non_zero / (taille_image[1]*taille_image[0]*3)) * 100 ) 
 print(f"taux de compression : {taux_compression}")
@@ -86,21 +82,25 @@ print(f"taux de compression : {taux_compression}")
 ###### CALCUL DE L'ERREUR ########
 erreur = np.linalg.norm((image-compressed))/(np.linalg.norm(image))
 print(f"l'erreur est : {erreur}")
+
 ####### DECOMPRESSION ###########
 decompressed = np.zeros_like(image, dtype=np.float32)
 
-for c in range(taille_image[2]):
+for c in range(taille_image[2]):  
     for i in range(0, taille_image[0], 8):
         for j in range(0, taille_image[1], 8):
             B = compressed[i:i+8,j:j+8,c]
+            #operations inverse à la compression
             D_tilde = B * Q
             D = np.dot(P.T, np.dot(D_tilde,P))
             D=np.round(D)
             decompressed[i:i+8, j:j+8, c] = D +128
-         
-            
+
 
 decompressed = np.clip(decompressed, 0, 255).astype(np.uint8)
+
+
+#Affichage des résulats 
 
 fig,axes = plt.subplots(2,2)
 axes[0, 0].imshow(image_origine)  
