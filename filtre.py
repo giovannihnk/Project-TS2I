626c03d3725189640ac2ba50b95b97dba21d7c82
emnabnk <153511857+emnabnk@users.noreply.github.com>
2025-01-16 18:18:00 +0100

    Update filtre.py

--- filtre.py
+++ filtre.py
@@ -7,33 +7,33 @@ import matplotlib.image as mpimg
 ###### INITIALISATION #######
 
 image_path = 'papillon.jpeg'
-
-image_origine = mpimg.imread(image_path)
+#lecture de l'image
+image_origine = mpimg.imread(image_path)  
 image = mpimg.imread(image_path)
 
-
+#Conversion des valeurs entre 0 et 255
 if image.dtype != np.uint8:  # Si les valeurs sont en flottant (0 à 1)
-    image = (image * 255) # Convertir en entiers (0 à 255)
+    image = (image * 255) 
 else:
     image = (image/np.max(image))*255
 
+
+
+# On tronque l'iamge avec ses nouvelles dimensions
 hauteur, largeur = image.shape[:2]
 nouvelle_hauteur = (hauteur // 8) * 8
 nouvelle_largeur = (largeur // 8) * 8
+image = image[:nouvelle_hauteur, :nouvelle_largeur,:]
+
+#centralise les valeurs dans la matrice
+image = image - 128
+
+taille_image = image.shape
 
 #seuil du filtre
 SEUIL=6
 
-# image = image[:,:,0]  #on garde qu'une seule couleur (2D)
 
-
-# Tronquer l'image
-image = image[:nouvelle_hauteur, :nouvelle_largeur,:]
-
-image = image - 128
-taille_image = image.shape
-#print(taille_image)
-#print(image)
 
 #initialisation de P (matrice de passage) avec la formule de la double somme
 P=np.zeros((8,8))
@@ -47,7 +47,7 @@ for i in range (8):
 
 
 
-# initialisation de Q : matrice de quantification Q dans la norme de compression JPEG
+# initialisation de Q : matrice de quantification Q 
 Q=np.array([[16,11,10,16,24,40,51,61],
             [12,12,13,19,26,58,60,55],
             [14,13,16,24,40,57,69,56],
@@ -64,11 +64,11 @@ Q=np.array([[16,11,10,16,24,40,51,61],
 
 compressed = np.zeros_like(image, dtype=float)
 
-for c in range(taille_image[2]):
-    for i in range(0, taille_image[0], 8):
+for c in range(taille_image[2]):  #compressions pour tous les canaux
+    for i in range(0, taille_image[0], 8):   #compression par bloc de 8x8
         for j in range(0, taille_image[1], 8):
-
             bloc = image[i:i+8, j:j+8,c]
+            #application du changement de base
             D = np.dot(P, np.dot(bloc,P.T))
             D_tilde = np.round(D/Q)
             compressed[i:i+8, j:j+8, c] = D_tilde
@@ -80,13 +80,6 @@ for c in range(taille_image[2]):
                         D_tilde[k, l] = 0
             compressed[i:i+8, j:j+8, c] = D_tilde
                        
-                          
-
-
-#  — Compter le nombre de cœfficients non nuls pour obtenir le taux de compression
-nb_coeff_non_zero = np.count_nonzero(compressed)  # Nombre de coefficients non nuls
-taux_compression = 100 - ((nb_coeff_non_zero / (taille_image[1]*taille_image[0]*3)) * 100 ) 
-print(f"taux de compression : {taux_compression}")
 
 
 ####### DECOMPRESSION ###########
@@ -96,19 +89,27 @@ for c in range(taille_image[2]):
     for i in range(0, taille_image[0], 8):
         for j in range(0, taille_image[1], 8):
             B = compressed[i:i+8,j:j+8,c]
+            #opérations inverse à la compression
             D_tilde = B * Q
             D = np.dot(P.T, np.dot(D_tilde,P))
             D=np.round(D)
             decompressed[i:i+8, j:j+8, c] = D +128
-         
-        
-decompressed = np.clip(decompressed, 0, 255).astype(np.uint8)
+
+#on limite les valeurs de la matrice entre 0 et 255
+decompressed = np.clip(decompressed, 0, 255).astype(np.uint8)  
+
+
+######## TAUX DE COMPRESSION ########
+nb_coeff_non_zero = np.count_nonzero(compressed)  # Nombre de coefficients non nuls
+taux_compression = 100 - ((nb_coeff_non_zero / (taille_image[1]*taille_image[0]*3)) * 100 ) 
+print(f"taux de compression : {taux_compression}")
+
 
 ###### CALCUL DE L'ERREUR ########
 erreur = (np.linalg.norm(((image+128)-decompressed))/(np.linalg.norm(image))) *100
 print(f"l'erreur est : {erreur}")
 
-
+#affichage des images
 fig,axes = plt.subplots(2,2)
 axes[0, 0].imshow(image_origine)  
 axes[0, 0].set_title("Image d'origine")  
